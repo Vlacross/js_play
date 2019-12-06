0607e89776dd144a9e62298866ac4a3ec4371b38
Tim Vlasuk <timvlasuk@gmail.com>
2019-12-06 08:39:59 -0800

    coding/following along with Javascript-Promise from scratch guide to refresh and solidify understanding

---
+++ promise_from_scratch.js
@@ -0,0 +1,27 @@
+/*  coding along with a 'Make Your Own Promise' guide.
+source: https://thecodebarbarian.com/write-your-own-node-js-promise-library-from-scratch.html */
+
+
+
+class customPromise {
+
+  constructor(executor) {
+
+  }
+
+  then(onFullfilled, onRejected) {
+
+
+  }
+
+  
+}
+
+
+
+
+
+
+
+
+
