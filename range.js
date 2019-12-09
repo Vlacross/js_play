6ab742f7e21d31e992acb14edaba1f54c0df3803
Tim Vlasuk <timvlasuk@gmail.com>
2019-12-09 14:01:21 -0800

    wrote Range funcion from scratch (hopefully handling all range error cases)

---
+++ range.js
@@ -0,0 +1,39 @@
+function Range(start, end, step=(start > end ? -1 : 1)) {
+
+  let deNegate = num => num < 0 ? ((num)*-1): num;
+
+  withinRange = (start, end, step ) => 1 < 1 + step ? 
+  (end - start) >= deNegate(step) :
+   (end + start) >= deNegate(step);
+
+  if(start > end && step >= 0) {
+    return "Cannot create decremented range with positive step."
+  }
+  else if (start < end && step <= 0) {
+    return "Cannot create incremental range with negative step."
+  }
+  else if (!withinRange(start, end, step)) {
+    return "The numbers just don't add up."
+  }
+  else  if (start === end) {
+    return [start];
+  }
+
+
+  let rangeArr = [];
+
+  let loopEnd = i => start > end ? i >= end : i <= end;
+  
+  for (i = start; loopEnd(i); i+=step) {
+    rangeArr.push(i)
+  };
+  return rangeArr;
+}
+
+/*
+Side tools
+  let least = (start, end) => start < end ? start : end;
+  let most = least(end, start);
+  let deNegate = end < 0 ? ((end)*-1): end;
+  let direction = 1 < 1 + step ? '-' : '+';
+*/
