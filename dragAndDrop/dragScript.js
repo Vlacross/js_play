2615036f8f9882dc013f038dda1e3303caabb969
Tim Vlasuk <timvlasuk@gmail.com>
2020-02-24 08:17:07 -0800

    added drop function with visual support; added color-wheel cycle on drag events

--- dragAndDrop/dragScript.js
+++ dragAndDrop/dragScript.js
@@ -4,8 +4,51 @@ function onDragStart(e) {
     .dataTransfer
     .setData('text/plain', e.target.id);
 
+  let toggledColor;
+
+  switch(e.currentTarget.style.backgroundColor) {
+    case 'red':
+      toggledColor = 'orange';
+      break;
+    case 'orange':
+      toggledColor = 'yellow';
+      break;
+    case 'yellow':
+      toggledColor = 'green';
+      break;
+    case 'green':
+      toggledColor = 'blue';
+      break;
+    case 'red':
+      toggledColor = 'red';
+      break;
+    default:
+      toggledColor = 'red'
+  };
+
   e
     .currentTarget
     .style
-    .backgroundColor = 'yellow';
+    .backgroundColor = toggledColor;
 }
+
+function onDragOver(e) {
+  e.preventDefault();
+}
+
+function onDrop(e) {
+  const id = event
+    .dataTransfer
+    .getData('text');
+
+  const draggableElement = document.getElementById(id);
+  const dropzone = event.target;
+
+  dropzone.appendChild(draggableElement);
+
+  event
+    .dataTransfer
+    .clearData();
+}
+
+
