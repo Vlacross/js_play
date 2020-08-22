dc2db23c2d60c79c6c1ab83189caeafb34cc7ce0
Tim Vlasuk <timvlasuk@gmail.com>
2020-08-21 17:36:04 -0700

    made working super-prime function

--- find_prime.js
+++ find_prime.js
@@ -47,6 +47,70 @@ function superPrimer(arr) {
   return superPrimes;
 };
 
+  
+/*
+ accept arg to set number or superprimes to return - default to 20;
+  Build prime number list
+    check that list contains at least as many indexes as the value of the index that matches requested return quantity
+  iterate through arr and pull vals
+
+  set a check at the end of prime builder that says for any number after 11: iterate by 2 
+*/
+ 
+function superPrimerFinder(num = 20) {
+  if(typeof(num) != 'number') {
+      console.log("input must be an integer")
+      return
+  }
+  num = Math.floor(num);
+  if (num < 0) {
+      num = 0 - num;
+  }
+
+  let i, j, k, tmp, triggered;
+  let ps = 1;
+  let sps = 0;
+  let primes = [];
+  let superPrimes = [];
+
+  function buildSet(start, arr) {
+
+      for(i=start; i < start + 20;  i < 11 ? i++ : i+=2) {
+          if (i==1) { continue }
+          j = 2;
+          triggered = false;
+          while (j <= (i/2)) {
+              if (i % j == 0) {
+                  triggered = true;
+                  break;
+              }
+              j < 11 ? j++ : j+=2;
+          }
+      
+          if (!triggered) {
+              arr.push(i)
+          }
+      }
+      ps = i;
+      primes = arr;
+  }
+
+  while (primes.length < num) {
+      buildSet(ps, primes)
+  }
+
+  tmp = primes[primes.length-1]
+  while (primes.length < tmp) {
+    buildSet(ps, primes)
+  }
+      
+  for (k = sps; k < num; k++) {
+    superPrimes.push(primes[primes[k]-1])
+  }
+  
+  return superPrimes;
+};
+
 
 
 function findPrimes(end, start = arguments.length === 2 ? start : 1) {
